9ca03597f9658021ac95b3b447eaa99b3b969b5c
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-07-18 14:19:03 +0300

    daily done

---
+++ w2d3/daily.py
@@ -0,0 +1,42 @@
+# 1)
+
+word = input("Enter a word: ")
+letter_indexes = {}
+
+for index, letter in enumerate(word):
+    if letter in letter_indexes:
+        letter_indexes[letter].append(index)
+    else:
+        letter_indexes[letter] = [index]
+
+print(letter_indexes)
+
+# 2)
+
+items_purchase = {
+    "Water": "$1",
+    "Bread": "$3",
+    "TV": "$1,000",
+    "Fertilizer": "$20",
+    "Apple": "$4",
+    "Honey": "$3",
+    "Fan": "$14",
+    "Bananas": "$4",
+    "Pan": "$100",
+    "Spoon": "$2"
+}
+
+wallet = input("Enter the amount of money in your wallet: ")
+
+affordable_items = []
+
+for item, price in items_purchase.items():
+    item_price = int(price.strip("$").replace(",", ""))
+    if item_price <= int(wallet.strip("$")):
+        affordable_items.append(item)
+
+if affordable_items:
+    affordable_items.sort()
+    print(affordable_items)
+else:
+    print("Nothing")
