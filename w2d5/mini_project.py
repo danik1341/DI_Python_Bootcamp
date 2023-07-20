f220af0fa51ecd60e2490451fb6fc5dd13933759
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-07-20 11:03:12 +0300

    a more proper documentary

--- w2d5/mini_project.py
+++ w2d5/mini_project.py
@@ -2,8 +2,14 @@
 
 def disply_board(board):
     '''
-        This function is responsible for displaying the current state of the Tic Tac Toe board. 
-        It takes the board list as input and prints the board with the marks of each player (X or O) at the corresponding positions.
+        Display the Tic Tac Toe board.
+
+        Parameters:
+        board (list): The Tic Tac Toe board represented as a list.
+
+        Returns:
+        None
+
     '''
     print(f'{board[0]} | {board[1]} | {board[2]}')
     print("--+---+--")
@@ -14,8 +20,14 @@ def disply_board(board):
 
 def player_input(player):
     '''
-        This function takes the player (X or O) as input and prompts them to enter a position (a number from 1 to 9). 
-        The function then returns the index in the board list where the player's mark should be placed.
+        Get the player's move.
+
+        Parameters:
+        player (str): The player's mark (X or O).
+
+        Returns:
+        int: The index where the player's mark should be placed on the board.
+
     '''
     posotion = int(input(f'Player {player}, enter a position (1-9) ')) - 1
     return posotion
@@ -23,11 +35,15 @@ def player_input(player):
 
 def check_win(board, player):
     '''
-        This function checks whether the player has won the game. 
-        It takes the current board and the player's mark as inputs. 
-        The function defines all possible winning combinations (rows, columns, and diagonals) and checks if any of them contain the player's mark in all three positions. 
-        If a winning combination is found, the function returns True, indicating that the player has won. 
-        Otherwise, it returns False.
+        Check if the player has won the game.
+
+        Parameters:
+        board (list): The current Tic Tac Toe board represented as a list.
+        player (str): The player's mark (X or O).
+
+        Returns:
+        bool: True if the player has won, False otherwise.
+
     '''
     win_combinations = [
         [0, 1, 2], [3, 4, 5], [6, 7, 8],
@@ -44,14 +60,14 @@ def check_win(board, player):
 
 def play():
     '''
-        This is the main function that orchestrates the game. 
-        It initializes the board as a list with empty spaces, sets up the players as "X" and "O", 
-        and starts the game loop that runs for a maximum of 9 turns (the total number of cells on the board). 
-        In each turn, it displays the board, gets the player's input for their move, 
-        and updates the board with the player's mark at the chosen position. 
-        After each move, it checks if the current player has won using the check_win() function. 
-        If there is a winner, it displays the winning board and the player's mark. 
-        If there is no winner after 9 turns, it declares the game as a tie.
+        Main function to play the Tic Tac Toe game.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+
     '''
     board = [" " for _ in range(9)]
     players = ['X', '0']
