2b586649b5bbe8b90042d9dc50e9baa629aeed27
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-07-20 11:24:56 +0300

    advanced added

--- w2d5/daily.py
+++ w2d5/daily.py
@@ -1,3 +1,5 @@
+import random
+
 # 1)
 
 intput_str = input('Enter comma-separated words: ')
@@ -17,3 +19,26 @@ def longest_word(sentence):
     words = sentence.split()
     longest = max(words, key=len)
     return longest
+
+
+# ADVANCED
+
+list_of_numbers = [random.randint(0, 10000) for _ in range(20000)]
+target_number = 3728
+
+
+def find_number_of_couples(numbers, target):
+    count = 0
+    seen = set()
+
+    for num in numbers:
+        complement = target - num
+        if complement in seen:
+            count += 1
+        seen.add(num)
+
+    return count
+
+
+result = find_number_of_couples(list_of_numbers, target_number)
+print(f"Number of couples of numbers that sum to {target_number}: {result}")
