9bd75b749fa06e8bfb7d1deeb9861d98bce9a1c6
danik1341 <111450120+danik1341@users.noreply.github.com>
2023-08-17 14:43:01 +0300

    ok now Im really done

--- w6d5/exam/academy/academy/school/admin.py
+++ w6d5/exam/academy/academy/school/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Course, Teacher
+from .models import Course, Teacher, SchoolFacility, Laboratory
 
 # Register your models here.
 
 admin.site.register(Course)
 admin.site.register(Teacher)
+admin.site.register(SchoolFacility)
+admin.site.register(Laboratory)
--- w6d5/exam/academy/academy/school/serializers.py
+++ w6d5/exam/academy/academy/school/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Teacher
+from .models import Teacher, SchoolFacility, Laboratory
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
